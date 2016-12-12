8af4437cf94363288537d45c01b0f80fe9727788
Chris Hinds <chris.hinds.github@gmail.com>
2016-12-12 11:39:47 +0000

    Added missing git prompt script. Fixed Windows line end bug in vim plugins script.

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
