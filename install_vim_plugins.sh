5328caad5247ce62e715318d0fec6c225b81e449
Chris Hinds <chris.hinds.github@gmail.com>
2016-12-27 06:49:09 +0000

    Updated dotfiles - 2016.12.27

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -8,5 +8,8 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
 git clone https://github.com/Yggdroot/indentLine.git ~/.vim/bundle/indentLine
+git clone https://github.com/alvan/vim-closetag.git ~/.vim/bundle/vim-closetag
+git clone https://github.com/sheerun/vim-polyglot.git ~/.vim/bundle/vim-polyglot
+git clone https://github.com/tpope/vim-abolish.git ~/.vim/bundle/vim-abolish
 
 wget https://raw.githubusercontent.com/tpope/vim-vividchalk/master/colors/vividchalk.vim -O ~/.vim/colors/vividchalk.vim
